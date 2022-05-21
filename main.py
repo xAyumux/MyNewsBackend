95518f5ee9c722aa1ee6d53ff653af67859ce041
xAyumux <ayumu.f.1127@gmail.com>
2022-05-21 18:31:04 +0900

    Fix main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from bs4 import BeautifulSoup
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:3000",
-# ]
+origins = [
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
