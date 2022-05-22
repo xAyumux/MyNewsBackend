b71e75dc2ef8d7a891da397b54dca8856b68818f
xAyumux <ayumu.f.1127@gmail.com>
2022-05-22 12:11:55 +0900

    Add feature search_keywords

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 
 class Keywords(BaseModel):
-    word: List
+    keywords: List
 
 
 @app.get("/hello")
@@ -85,5 +85,51 @@ def get_gizmodo():
 
 
 @app.post("/articles/keywords")
-def search_keywords():
-    return {"title": "title"}
+def search_keywords(keywords: Keywords):
+    # yahoo
+    load_url = "https://news.yahoo.co.jp/ranking/access/news"
+    html = requests.get(load_url)
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    count = 1
+
+    articlesDict = {}
+
+    topic = soup.find(class_="newsFeed_list")
+    for element in topic.find_all("a"):
+        if(count < 9):
+            rank = element.text[0:1]
+            title = element.text[1:]
+        else:
+            rank = element.text[0:2]
+            title = element.text[2:]
+
+        url = element.get("href")
+
+        for keyword in keywords.keywords:
+            if keyword in title:
+                articleDict = {"rank": rank, "title": title, "url": url}
+                articlesDict[count] = articleDict
+
+        count += 1
+
+    # gizmodo
+    url = "https://www.gizmodo.jp/"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    elems = soup.find_all(href=re.compile("https://www.gizmodo.jp/2022/05"))
+    for elem in elems:
+        print("title : "+elem.contents[0].text)
+        print("url : "+elem.attrs['href'])
+        title = elem.contents[0].text
+        url = elem.attrs['href']
+
+        for keyword in keywords.keywords:
+            if keyword in title:
+                articleDict = {"title": title, "url": url}
+                articlesDict[count] = articleDict
+
+        count += 1
+
+    return articlesDict
