5cfdb8296af4ad6fb03bb390123868545f5bb436
xAyumux <ayumu.f.1127@gmail.com>
2022-05-22 14:57:13 +0900

    Fix yahoo_ranking scraping

--- scraping.py
+++ scraping.py
@@ -59,26 +59,52 @@ def yahoo_ranking():
     load_url = "https://news.yahoo.co.jp/ranking/access/news"
     html = requests.get(load_url)
     soup = BeautifulSoup(html.content, "html.parser")
+    elems = soup.find_all(href=re.compile("news.yahoo.co.jp/article"))
+
+    topic = soup.find_all(href=re.compile("news.yahoo.co.jp/articles"))
+    topic_links = [elem.attrs['href'] for elem in elems]
 
     count = 1
 
     articles_dict = {}
 
-    topic = soup.find(class_="newsFeed_list")
-    for element in topic.find_all("a"):
-        if(count < 9):
-            rank = element.text[0:1]
-            title = element.text[1:]
-        else:
-            rank = element.text[0:2]
-            title = element.text[2:]
+    # 一覧のリンクを順に処理
+    for topic_link in topic_links:
 
-        url = element.get("href")
+        # ランキングを記録
+        for i in range(0, 40):
+            Rank = i
+            Rank += 1
+            rank = Rank
 
-        article_dict = {"rank": rank, "title": title, "url": url}
-        articles_dict[count] = article_dict
+            # topicページへ遷移しページの情報を取得
+            topic_res = requests.get(topic_link)
+            topic_soup = BeautifulSoup(topic_res.text, "html.parser")
 
-        count += 1
+            # ニュースページへのリンクを取得
+            topic_elem = topic_soup.find(
+                "meta", content=re.compile("news.yahoo.co.jp/articles"))
+            news_link = topic_elem.attrs["content"]
+
+            # ニュースページの情報を取得
+            news_res = requests.get(news_link)
+            news_soup = BeautifulSoup(news_res.text, "html.parser")
+
+            # タイトルとURLを表示
+            title = topic_soup.title.text
+            url = news_link
+
+            # ニュースのサムネイル&テキスト情報を取得し表示
+            img_res = news_soup.find(type="image/jpeg")
+            img_url = img_res.attrs['srcset']
+            Text = news_soup.find(class_=re.compile("highLightSearchTarget"))
+            snippet = Text.text
+
+            article_dict = {"rank": rank, "title": title,
+                            "url": url, "img_url": img_url, "snippet": snippet}
+            articles_dict[count] = article_dict
+
+            count += 1
 
     return articles_dict
 
